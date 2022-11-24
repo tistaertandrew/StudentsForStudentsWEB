5b569189e8e105a702d3978a9e917a6a9d2d5c49
Andrew Tistaert <a.tistaert@student.helmo.be>
2022-11-24 14:24:29 +0100

    MODIFY: fin de la page d'accueil

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,35 +3,56 @@ import {observer} from "mobx-react";
 import {homeStore} from "../../stores/HomeStore";
 import {useEffect} from "react";
 import LeftImage from '../../assets/images/tired.png'
+import {useNavigate} from "react-router-dom";
+import routes from '../../routes.json'
+import {sessionStore} from "../../stores/SessionStore";
 
 function Home() {
+    const navigate = useNavigate()
+
     useEffect(async () => {
         await homeStore.init()
     }, [])
 
+    const navigateToAuthentication = () => {
+        navigate(routes.Authentication)
+    }
+
+    const navigateToAbout = () => {
+        navigate(routes.About)
+    }
+
+    const navigateToSyntheses = () => {
+        navigate(routes.Syntheses)
+    }
+
     return (
         <div>
-            <ObservedNavBar />
+            <ObservedNavBar/>
             <div className={'home-main-container'}>
                 <div className={'column'} style={{justifyContent: 'center'}}>
-                    <h1 className={'home-main-title'}>Students for Students</h1>
-                    <h2 className={'home-main-p'}>Une plateforme pour les étudiants, faites par les étudiants</h2>
+                    <h1 className={'home-main-title'}>Students for Students.</h1>
+                    <h2 className={'home-main-p'}>Une plateforme pour les étudiants, faites par les étudiants.</h2>
                     <div className={'row'}>
                         <div className={'column'}>
-                            <h1 className={'home-main-key'}>{homeStore.usersCount}</h1>
+                            <h1 className={'home-main-key'}>{homeStore.usersCount < 9 ? `0${homeStore.usersCount}` : homeStore.usersCount}</h1>
                             <h2 className={'home-main-value'}>Utilisateur(s) inscrit(s)</h2>
                         </div>
                         <div className={'column'}>
-                            <h1 className={'home-main-key'}>{homeStore.filesCount}</h1>
+                            <h1 className={'home-main-key'}>{homeStore.filesCount < 9 ? `0${homeStore.filesCount}` :
+                                <homeStore className="gi"></homeStore>}</h1>
                             <h2 className={'home-main-value'}>Synthèse(s) disponible(s)</h2>
                         </div>
                     </div>
                     <div className={'row'}>
                         <div className={'column'}>
-                            <input type={'submit'} className={'btn-home-green'} value={'SE CONNECTER'}/>
+                            <input type={'submit'} className={'btn-home-green'}
+                                   value={sessionStore.user ? 'VOIR LES SYNTHESES' : 'SE CONNECTER'}
+                                   onClick={sessionStore.user ? navigateToSyntheses : navigateToAuthentication}/>
                         </div>
                         <div className={'column'}>
-                            <input type={'submit'} className={'btn-home-brown'} value={'PLUS D\'INFORMATIONS'}/>
+                            <input type={'submit'} className={'btn-home-brown'} value={'PLUS D\'INFORMATIONS'}
+                                   onClick={navigateToAbout}/>
                         </div>
                     </div>
                 </div>
