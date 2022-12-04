49d4a9d1dafacbcd0aa5ce99c99a8d9343bc0884
Andrew Tistaert <a.tistaert@student.helmo.be>
2022-12-04 12:15:19 +0100

    MODIFY: ajout message si aucun utilisateur ne correspond à un filtre

--- src/components/organisms/DisplayUsers.js
+++ src/components/organisms/DisplayUsers.js
@@ -1,6 +1,14 @@
 import {DisplayUser} from "../molecules/DisplayUser";
+import EmptyContent from "../molecules/EmptyContent";
 
 export default function DisplayUsers({handleEdit, handleBlock, handleDelete, users}) {
+
+    if(users.length === 0) {
+        return (
+            <EmptyContent message={'Aucun utilisateur correspondant'}/>
+        )
+    }
+
     return (
         <div className={'file-container'}>
             {users.map((user) => <DisplayUser firstname={user.firstname} lastname={user.lastname} email={user.email}
